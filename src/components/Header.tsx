1f51cfb856174cd655c51ef2b73ec30c383c1199
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-11-26 11:23:46 -0400

    task: Add basic header component with logo

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="flex justify-between items-center text-2xl py-3">
+      <h2 className="text-white font-arvo text-3xl font-extrabold tracking-wider">
+        Pix<span className="text-primary">Minify</span>
+      </h2>
+    </header>
+  )
+}
