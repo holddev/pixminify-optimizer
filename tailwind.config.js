3ef5fa3f0fb2494f427af8fdb60fa64e969b4773
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-12-08 13:40:36 -0400

    chore: add custom classes to tailwind css configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: '#4bff0a',
+        white: '#eeffe4'
+      },
+      fontFamily:{
+        arvo: ['Arvo', 'sans serif', 'Times New Roman']
+      },
+      borderRadius: {
+        '50p': '50%'
+      }
+    },
   },
   plugins: [],
 }
