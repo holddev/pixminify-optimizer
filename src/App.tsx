a8409bf520bec442da0e6d12c1ff46e0a91eed2e
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-12-08 12:43:26 -0400

    feat: add router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { Optimize } from "./pages/Optimize"
+
+
 function App() {
 
   return (
     <>
-
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/optimize" element={<Optimize />} />
+        </Routes>
+      </Router>
     </>
   )
 }
