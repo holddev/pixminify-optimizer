916f5de21ff6489fe55f859d6bc98952b70f742c
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-02-09 17:13:26 -0400

    feat: Import Pixel-Canvas web component in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { Optimize } from "./pages/Optimize"
+import '../src/components/web-component/Pixel-Canvas.js'
 
 
 function App() {
