7d3893783020e46dc0621254c5ebc4f9cce3daa3
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-02-09 17:13:58 -0400

    feat: Integrate ThemeSwitcher component into Header for theme management

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
+import { ThemeSwitcher } from './ThemeSwitcher'
+
 export const Header = () => {
+
   return (
     <header className="flex justify-between items-center text-2xl py-3">
-      <h2 className="text-white font-arvo text-3xl font-extrabold tracking-wider">
+      <h2 className="text-light-text_primary dark:text-dark-text_primary
+      font-arvo text-3xl font-extrabold tracking-wider">
         Pix<span className="text-primary">Minify</span>
       </h2>
+      <ThemeSwitcher />
     </header>
   )
 }
