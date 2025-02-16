c52c0f5fed456e560bd50e7f128934fc9b6c3ffd
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-02-16 17:32:44 -0400

    Task: Add the maximum length of images allowed and limit formats for optimization to utils

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -5,3 +5,12 @@ export const formatFileSize = (sizeInBytes: number) => {
   const mb = kb / 1024;
   if (mb < 1024) return `${mb.toFixed(1)} MB`;
 }
+
+export const formatFiles = [
+  { name: 'WEBP', value: 'image/webp' },
+  { name: 'JPEG', value: 'image/jpeg' },
+  { name: 'PNG', value: 'image/png' },
+  { name: 'BMP', value: 'image/bmp' },
+]
+
+export const MAX_LENGTH_IMAGES = 20
