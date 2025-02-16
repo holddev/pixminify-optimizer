52212439cd20e7440b2610404703a9859a1e6e83
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-02-16 17:25:23 -0400

    task: Add chrevronDown icon in icons component

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -96,3 +96,12 @@ export const Download: React.FC<Props> = ({ class: className }) => {
   )
 }
 
+export const ChevronDown: React.FC<Props> = ({ class: className }) => {
+  return (
+    <svg className={className} width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M6 9l6 6l6 -6" />
+    </svg>
+  )
+}
+
