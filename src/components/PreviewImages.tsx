aa54d68efd3776bd2eee2d4e0d97df34cb7ef171
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-02-15 21:59:23 -0400

    feat: Integrate image compression function and download functionality to PreviewImages component

--- src/components/PreviewImages.tsx
+++ src/components/PreviewImages.tsx
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { Image, ImageID } from "../types/types";
 import { PreviewImageItem } from "./PreviewImagesItem";
+import { compressImage } from "../lib/ImageCompression";
+import { useState } from "react";
+import { Download } from "./Icons";
+import JSZip from "jszip";
 
 interface Props {
   images: Image[]
@@ -8,6 +12,38 @@ interface Props {
 }
 
 export const PreviewImagesList: React.FC<Props> = ({ images, handleOnDelete }) => {
+
+  const [imagesCompressed, setImagesCompresed] = useState<File[]>([])
+
+
+  const handleOnCompressImage = async (image: Image) => {
+    const imageCompressed = await compressImage({ imageFile: image.image })
+    if (!imageCompressed) return
+    setImagesCompresed((prev) => {
+      return [...prev,
+        imageCompressed
+      ]
+    })
+    return imageCompressed
+  }
+
+  const handleDowmloadAllImages = async () => {
+    const zip = new JSZip()
+    imagesCompressed.forEach((image) => {
+      zip.file(image.name, image);
+    });
+    const zipBlob = await zip.generateAsync({ type: "blob" });
+
+    const zipUrl = URL.createObjectURL(zipBlob);
+    const a = document.createElement("a");
+    a.href = zipUrl;
+    a.download = "images.zip";
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    URL.revokeObjectURL(zipUrl);
+  }
+
 
   const navigate = useNavigate()
 
@@ -19,12 +55,29 @@ export const PreviewImagesList: React.FC<Props> = ({ images, handleOnDelete }) =
     <>
       <section className="h-[500px] py-4  dark:bg-dark-primary_variant1 mt-8 
         flex flex-col items-center justify-start gap-6 text-black overflow-hidden transition-shadow duration-300
-        rounded-lg shadow-primary shadow-sm"
+        rounded-lg shadow-primary shadow-sm px-10"
       >
-        <ul className="flex flex-col gap-2 w-4/5 text-light-text_primary dark:text-dark-text_primary overflow-y-auto">
+        <div className="w-full flex justify-end">
+          <button
+            onClick={handleDowmloadAllImages}
+            className="flex items-center gap-2
+            py-2 px-4 rounded-md border-2 border-solid 
+            bg-black/60 text-primary border-primary hover:bg-primary/70 
+            dark:hover:text-dark-text_primary hover:text-light-text_primary 
+            dark:hover:border-dark-text_primary hover:border-light-text_primary"
+          >
+            <Download class="size-5" />Descargar en zip
+          </button>
+        </div>
+        <ul className="flex flex-col gap-2 w-full text-light-text_primary dark:text-dark-text_primary overflow-y-auto">
           {images.map((image) => {
             return (
-              <PreviewImageItem item={image} key={image.id} onDelete={handleOnDelete} />
+              <PreviewImageItem
+                item={image}
+                key={image.id}
+                onDelete={handleOnDelete}
+                onCompressImage={handleOnCompressImage}
+              />
             )
           })}
         </ul>
