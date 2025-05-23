54ec6dc2fcb44b30df97cf27e608247e0b274832
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-05-23 14:29:49 -0400

    fix: adjust header styles for improved layout and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import { ThemeSwitcher } from './ThemeSwitcher'
 export const Header = () => {
 
   return (
-    <header className="flex justify-between items-center text-2xl py-3">
+    <header className="w-full flex justify-between items-center text-2xl py-3 px-2 md:px-0">
       <h2 className="text-light-text_primary dark:text-dark-text_primary
-      font-arvo text-3xl font-extrabold tracking-wider">
+      font-arvo text-2xl md:text-3xl font-extrabold tracking-wider">
         Pix<span className="text-primary">Minify</span>
       </h2>
       <ThemeSwitcher />
