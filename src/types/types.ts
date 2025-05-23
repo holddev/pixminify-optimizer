b0806e56f92d0a412a3c3d9dda949071330c1a7e
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-05-23 14:29:05 -0400

    feat: add CompressionSettings interface to define image compression options

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,13 @@
+export interface CompressionSettings {
+  saveMetadata?: boolean
+  format?: 'jpeg' | 'png' | 'webp' | 'bmp'
+}
+
 export interface Image {
   id: `${string}-${string}-${string}-${string}-${string}`
   image: File
+  settings?: CompressionSettings
+  compressedImage?: File
 }
 
 export type ImageID = Pick<Image, 'id'>
