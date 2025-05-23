30b43fd129f05a6da57e2ac395d63595f8cbb211
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-05-23 14:41:57 -0400

    style: enhance UploadImage component layout and responsiveness

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -12,7 +12,6 @@ export const UploadImage = () => {
   const dragCounter = useRef(0);
   const dialogRef = useRef<HTMLDialogElement | null>(null)
 
-  console.log(images)
 
   const handleDrop = (e: React.DragEvent) => {
     e.preventDefault()
@@ -92,10 +91,10 @@ export const UploadImage = () => {
 
 
   return (
-    <main className="mb-12">
-      <form className="text-light-text_primary dark:text-dark-text_primary pt-20" >
+    <main className="mb-12 w-full">
+      <form className="text-light-text_primary dark:text-dark-text_primary pt-20 px-5 md:px-10 " >
         <div
-          className={`relative w-full h-[40vh] p-8 border-dashed border-[3px] rounded-full 
+          className={`relative w-full h-[250px] md:h-[40vh] p-8 border-dashed border-[3px] rounded-3xl md:rounded-full 
         text-center cursor-pointer flex items-center justify-center transition-colors duration-500
         ${isDrag ? 'bg-[#b5ff9031] border-white' : 'bg-gradient-to-b from-[#0000] to-[#b5ff901e] border-primary'}`}
           onClick={() => { }}
@@ -112,14 +111,16 @@ export const UploadImage = () => {
           />
 
           <label htmlFor="fileInput" className={`${isDrag && 'text-primary'}
-        text-xl flex flex-col items-center justify-center cursor-pointer`}>
-            {isDrag ?
-              "Suelta aqui la imagen"
-              : "Arrastra y suelta una imagen o haz clic para seleccionar"
-            }
+            w-full flex flex-col items-center justify-center cursor-pointer text-sm  md:text-xl`}>
+            <h4 >
+              {isDrag ?
+                "Suelta aqui la imagen"
+                : "Arrastra y suelta una imagen o haz clic para seleccionar"
+              }
+            </h4>
             <span className="pt-4 flex items-center justify-center gap-16">
               <Arrow class={`${isDrag ? 'text-white opacity-100 translate-x-0' : 'opacity-0 -translate-x-8'} rotate-180 transition-all duration-500`} />
-              <svg viewBox="0 0 24 24" className={`size-24 transition-opacity duration-500 ${isDrag ? 'opacity-0' : 'opacity-100 '}`} fill="none">
+              <svg viewBox="0 0 24 24" className={`size-20 md:size-24 transition-opacity duration-500 ${isDrag ? 'opacity-0' : 'opacity-100 '}`} fill="none">
                 <path className="text-primary" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 16c1.403-.234 3.637-.293 5.945.243M16 21c-1.704-2.768-4.427-4.148-7.055-4.757m0 0C10.895 13.985 14.558 12 21 12h1M8.5 7C8 7 7 7.3 7 8.5S8 10 8.5 10 10 9.7 10 8.5 9 7 8.5 7z" />
                 <path className="text-primary" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M22 12c0 5.523-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2m7 0v3m0 3V5m0 0h3m-3 0h-3" />
               </svg>
